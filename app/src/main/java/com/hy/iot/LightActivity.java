464e646a27596104feb4ecdaa18f0ba8fdffc626
huangye <huangye@cvte.com>
2021-11-16 12:57:57 +0800

    [feature]增加跑马灯代码。但是运行效果不好

---
+++ app/src/main/java/com/hy/iot/LightActivity.java
@@ -0,0 +1,69 @@
+package com.hy.iot;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author huangye
+ */
+public class LightActivity extends AppCompatActivity {
+
+    private static final String TAG = "@LightActivity";
+
+    private static final long INTERNAL = 100L;
+    private static final long MAX_INDEX = 5;
+
+    Button mButton1;
+    Button mButton2;
+
+    private LightController mLightController;
+
+    private final Handler mMainHandler = new Handler();
+    private Runnable mRunLightRunnable;
+    private int mIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_light);
+
+        mLightController = new LightController(this);
+
+        mButton1 = findViewById(R.id.button);
+        mButton2 = findViewById(R.id.button2);
+
+        mRunLightRunnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < MAX_INDEX; i++) {
+//                    mLightController.setLight(i, mIndex == i);
+                    mLightController.setLight(i, !(mIndex == i));
+                }
+                mIndex++;
+                if (mIndex >= MAX_INDEX) {
+                    mIndex = 0;
+                }
+                mMainHandler.postDelayed(mRunLightRunnable, INTERNAL);
+            }
+        };
+
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRunLightRunnable.run();
+            }
+        });
+
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMainHandler.removeCallbacksAndMessages(null);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/hy/iot/LightController.java
@@ -0,0 +1,52 @@
+package com.hy.iot;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManager;
+
+/**
+ * @author huangye
+ */
+public class LightController {
+
+    private static final String TAG = "@LightController";
+
+    private static final String[] LIGHTS = {
+            "BCM5",
+            "BCM6",
+            "BCM13",
+            "BCM19",
+            "BCM26",
+    };
+
+    private Context mContext;
+    private PeripheralManager mPeripheralManager;
+    private final Gpio[] mGpios = new Gpio[LIGHTS.length];
+
+    public LightController(Context context) {
+        this.mContext = context;
+        mPeripheralManager = PeripheralManager.getInstance();
+
+        for (int i = 0; i < LIGHTS.length; i++) {
+            try {
+                Gpio gpio = mPeripheralManager.openGpio(LIGHTS[i]);
+                gpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+                mGpios[i] = gpio;
+            } catch (Exception e) {
+                Log.e(TAG, "setLight error", e);
+            }
+        }
+    }
+
+    public void setLight(int index, boolean on) {
+        try {
+            mGpios[index].setValue(on);
+        } catch (Exception e) {
+            Log.e(TAG, "setLight error", e);
+        }
+
+    }
+
+}
